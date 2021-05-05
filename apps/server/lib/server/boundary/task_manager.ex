4d5315fe5889efd08a9928e959cf139dbb7946ae
Stephen Turley <stephen.turley@blockfi.com>
2021-05-05 12:01:45 -0400

    added a TaskManager

---
+++ apps/server/lib/server/boundary/task_manager.ex
@@ -0,0 +1,15 @@
+defmodule Server.Boundary.TaskManager do
+  use Agent
+
+  def start_link(_) do
+    Agent.start_link(fn -> [] end)
+  end
+
+  def all(manager) do
+    Agent.get(manager, &Function.identity/1)
+  end
+
+  def add(manager, task) do
+    Agent.update(manager, fn list -> [task | list] end)
+  end
+end
---
+++ apps/server/test/boundary/task_manager_test.exs
@@ -0,0 +1,23 @@
+defmodule Boundary.TaskManagerTest do
+  use ExUnit.Case, async: true
+
+  alias Server.Boundary.TaskManager
+  alias Server.Core.Task
+
+  setup do
+    task_manager = start_supervised!(TaskManager)
+    %{task_manager: task_manager}
+  end
+
+  test "starts with empty list", %{task_manager: task_manager} do
+    assert TaskManager.all(task_manager) == []
+  end
+
+  test "can add a new Task", %{task_manager: task_manager} do
+    TaskManager.add(task_manager, Task.new(title: "Flerpn"))
+
+    assert TaskManager.all(task_manager) == [
+             %Task{title: "Flerpn"}
+           ]
+  end
+end
